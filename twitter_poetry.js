77728ff814b58242ac90814e2e7d42fc6977a31d
Steve Hackbarth <shackbarth@Steves-MacBook-Pro.local>
2014-08-11 00:02:55 -0400

    use rhymes

--- twitter_poetry.js
+++ twitter_poetry.js
@@ -24,7 +24,7 @@ var isJunk = function (s) {
     !/^[\x00-\x7F]*$/.test(s);
 }
 var cleanTweet = function (tweet) {
-  tweetWords = tweet.split(" ");
+  tweetWords = tweet.split("/\s+/");
   while(true) {
     if (!tweetWords[0]) {
       //console.log("rejected to 0", tweet);
@@ -60,15 +60,34 @@ var options = {
   filter_level: "medium",
   track: "baby,body,boys,girls,happy,heart,home,kiss,lonely,love,money,rain,sad,sexy,smile"
 };
-twit.stream('statuses/filter', options, function (stream) {
-  stream.on('data', function (data) {
-    if (data.text) {
-      var tweet = cleanTweet(data.text);
-      if (tweet) {
-        console.log(tweet);
+var allTweets = [{tweet: "My cousin is covered in blue", lastWord: "blue"}];
+var rhyme = require('rhyme');
+rhyme(function (r) {
+  twit.stream('statuses/filter', options, function (stream) {
+    stream.on('data', function (data) {
+      if (data.text) {
+        var tweet = cleanTweet(data.text);
+        if (!tweet) {
+          return;
+        }
+        var tweetSyllables = hyphenate(tweet).length;
+        if (tweetSyllables < 10 || tweetSyllables > 16) {
+          return;
+        }
+        var tweetWords = tweet.split(" ");
+        var lastWord = tweetWords[tweetWords.length - 1];
+        var lastWordRhymes = r.rhyme(lastWord);
+        var rhymingTweet = _.find(allTweets, function (savedTweet) {
+          return _.contains(lastWordRhymes, savedTweet.lastWord.toUpperCase());
+
+        });
+        if (rhymingTweet) {
+          allTweets = _.without(allTweets, rhymingTweet);
+          console.log(tweet);
+          console.log(rhymingTweet.tweet);
+        }
+        allTweets.push({tweet: tweet, lastWord: lastWord});
       }
-      //console.log(JSON.stringify(parser.parse(tweet).children[0]));
-      //console.log(_.map(data.text.split(" "), hyphenate));
-    }
+    });
   });
 });
