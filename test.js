4fd997aa4b88aac2ba87ce140bb36ad5c2fb4bd4
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-08-12 00:51:08 +0000

    start on verse from input

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var gather = require("./lib/twitter_poetry").gatherVerse,
-  creds = require("./api_keys").keys;
+var gather = require("./lib/twitter_poetry").gatherVerse,//.buildVerseFromInput,
+  creds = require("./api_keys").keys,
+  line = "I'm a cowboy, on a steel horse I ride";
 
-gather({creds: creds}, function (err, res) {
+gather({creds: creds, input: line}, function (err, res) {
   console.log("done!", arguments);
 });
